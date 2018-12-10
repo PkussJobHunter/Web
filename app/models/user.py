fa2a66f42e0f8a73ea542366b47bb17e918316ac
owlsn <helloworld.zouhao@gmail.com>
2018-12-10 19:23:49 +0800

    render template

--- app/models/user.py
+++ app/models/user.py
@@ -1,2 +1,21 @@
+# coding = utf-8
+from app.extensions import db
 
 
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    gender = db.Column(db.Boolean, default=False)
+    status = db.Column(db.Boolean, default=False)
+    info = db.Column(db.String(255), nullable=True)
+    uid = db.Column(db.String(50), nullable=True, unique=True)
+    phone = db.Column(db.String(25), nullable=True, unique=True)
+
+    def __repr__(self):
+        return self.username
+
+    pass
--- app/views/pages/index.py
+++ app/views/pages/index.py
@@ -1,12 +1,18 @@
 from ...extensions import cache
 from flask import render_template
 from . import pages
+from app.models.user import User
 
 # 视图函数
 @pages.route('/', methods=['GET', 'POST'])
 # @cache.cached(timeout=90, key_prefix='post')
 def index():
-    return render_template('pages/index/index.html')
+    user = User()
+    list = user.query.all()
+    context = {
+        'list' : list
+    }
+    return render_template('pages/index/index.html', ** context)
     pass
 
 # 视图函数
